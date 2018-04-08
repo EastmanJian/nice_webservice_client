1406332fdcb1cbb6def62e30747d5e06a0b1284f
EastmanJian <50935@qq.com>
2018-04-09 01:37:04 +0800

    Use ObjectFactory

--- src/main/java/nice/Application.java
+++ src/main/java/nice/Application.java
@@ -1,16 +1,12 @@
 package nice;
 
-import nice.wsdl.ArrayOfKeyValuePair;
-import nice.wsdl.GeneralInfo;
-import nice.wsdl.KeyValuePair;
-import nice.wsdl.SetGeneralInfoResponse;
+import nice.wsdl.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import javax.xml.bind.JAXBElement;
-import javax.xml.namespace.QName;
 
 @SpringBootApplication
 public class Application {
@@ -22,25 +18,28 @@ public class Application {
     CommandLineRunner lookup(NiceClient niceClient) {
         return args -> {
             //construct the SOAP object
-            JAXBElement<String> jeKey = new JAXBElement<>(new QName("Key"), String.class, "key1");
-            JAXBElement<String> jeValue = new JAXBElement<>(new QName("Value"), String.class, "value1");
+            ObjectFactory objFac = new ObjectFactory();
+
+            JAXBElement<String> jeKey = objFac.createKeyValuePairKey("key1");
+            JAXBElement<String> jeValue = objFac.createKeyValuePairValue("value1");
             KeyValuePair kvp = new KeyValuePair();
             kvp.setKey(jeKey);
             kvp.setValue(jeValue);
 
             ArrayOfKeyValuePair aokvp = new ArrayOfKeyValuePair();
             aokvp.getKeyValuePair().add(kvp);
-            jeKey = new JAXBElement<>(new QName("Key"), String.class, "key2");
-            jeValue = new JAXBElement<>(new QName("Value"), String.class, "value2");
+            jeKey = objFac.createKeyValuePairKey("key2");
+            jeValue = objFac.createKeyValuePairValue("value2");
+            kvp = new KeyValuePair();
             kvp.setKey(jeKey);
             kvp.setValue(jeValue);
             aokvp.getKeyValuePair().add(kvp);
 
-            JAXBElement<ArrayOfKeyValuePair> jeaokvp = new JAXBElement<>(new QName("ArrayOfKeyValuePair"), ArrayOfKeyValuePair.class, aokvp);
+            JAXBElement<ArrayOfKeyValuePair> jeaokvp = objFac.createGeneralInfoKeyValuePairs(aokvp);
             GeneralInfo generalInfo = new GeneralInfo();
             generalInfo.setKeyValuePairs(jeaokvp);
 
-            JAXBElement<GeneralInfo> reqPara = new JAXBElement<>(new QName("GeneralInfo"), GeneralInfo.class, generalInfo);
+            JAXBElement<GeneralInfo> reqPara = objFac.createSetGeneralInfoGeneralInfo(generalInfo);
 
             //invoke the web service
             SetGeneralInfoResponse response = niceClient.setGeneralInfo(reqPara);
